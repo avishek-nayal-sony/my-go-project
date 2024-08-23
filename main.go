e5a2c8c852c7ca7020dc845ead72bc4243932cd5
avishek-nayal-sony <avishek.nayal@sony.com>
2024-08-23 13:20:04 +0530

    sql added

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
-package main
+package security
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    username := "admin"
+    var password = "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
+    // var password = os.Getenv("PASSWORD")
+    fmt.Println("Doing something with: ", username, password)
 }
